93126fd7e643c625047cfae291a69eb4cbec2958
Prestance1 <oudrhiri.amin@gmail.com>
2022-10-06 20:32:48 +0100

    :yawning_face: Solved

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,10 @@
+from collections import Counter
+from typing import Union, List, Dict
+
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+def frequencies(items: List[Union[str, int]]) -> Dict[str, int]:
+    return dict(Counter(str(item) for item in items))
+
+print(frequencies(['n', 'o', 'o', 'b']))
