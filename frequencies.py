5acdcd692d278dc42d60b3ad1027421db3a41473
prestance1 <oudrhiri.amin@gmail.com>
2022-10-07 22:54:38 +0100

    :sparkles: Type hinting

--- frequencies.py
+++ frequencies.py
@@ -1,8 +1,8 @@
 from collections import Counter
-from typing import Union, List, Dict
+from typing import Union, List, Mapping
 
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items: List[Union[str, int]]) -> Dict[str, int]:
+def frequencies(items: List[Union[str, int]]) -> Mapping[str, int]:
     return Counter(str(item) for item in items)
